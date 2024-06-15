b031096123acd35bba447bddcfe9c23124c74010
Morgan Roderick <morgan@roderick.dk>
2024-06-15 21:55:55 +0200

    Add dates for ECB, Euro

---
+++ data/ECB-2024.js
@@ -0,0 +1,11 @@
+// The data in this file is taken from ECB
+// The dates used are marked with `*` - TARGET closing day
+// https://www.ecb.europa.eu/ecb/contacts/working-hours/html/index.en.html
+module.exports = [
+  "2024-01-01",
+  "2024-03-29",
+  "2024-04-01",
+  "2024-05-01",
+  "2024-12-25",
+  "2024-12-26"
+];
---
+++ data/ECB-2025.js
@@ -0,0 +1,11 @@
+// The data in this file is taken from ECB
+// The dates used are marked with `*` - TARGET closing day
+// https://www.ecb.europa.eu/ecb/contacts/working-hours/html/index.en.html
+module.exports = [
+  "2025-01-01",
+  "2025-04-18",
+  "2025-04-21",
+  "2025-05-01",
+  "2025-12-25",
+  "2025-12-26"
+];
