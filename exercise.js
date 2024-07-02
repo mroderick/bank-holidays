2591f95a5d3e82545fc9165ddd8123783d5a40c7
Morgan Roderick <morgan@roderick.dk>
2024-07-02 18:35:39 +0200

    Remove data specific things

--- exercise.js
+++
@@ -1,7 +0,0 @@
-const isBankHolidayByCountry = require("./lib/is-bank-holiday-by-country");
-const isBankHolidayByCurrency = require("./lib/is-bank-holiday-by-country");
-
-const isHolsByCountry = isBankHolidayByCountry("DK", "2024-12-24");
-const isHolsByCurrency = isBankHolidayByCurrency("DKK", "2024-12-24");
-
-console.log(isHolsByCountry, isHolsByCurrency);
